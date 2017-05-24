4bdfcd20bfb07cf0dcf16881678c529e9acca187
ilumer <18888106831qxq@gmail.com>
2017-05-24 14:37:01 +0800

    完成练习3.08

---
+++ 3.08-Exercise-AddAFlavor-SpecificActivity/app/src/paid/java/com/udacity/gradle/flavorspecificactivity/paid/SecondACtivity.java
@@ -0,0 +1,13 @@
+package com.udacity.gradle.flavorspecificactivity.paid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.udacity.gradle.flavorspecificactivity.R;
+
+public class SecondACtivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_second);
+  }
+}
